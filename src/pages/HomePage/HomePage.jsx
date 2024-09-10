f117e18b2809d1bb1703b5bb45b7e52fedf4bb09
Sameer Singh <sameersingh1600@gmail.com>
2024-09-10 17:28:42 -0400

    Set up react router navigation

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './HomePage.scss';
+
+function HomePage() {
+    return (
+        <div>
+            <p>This is the HomePage</p>
+        </div>
+    );
+}
+
+export default HomePage;
---
+++ src/pages/TeamPage/TeamPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './TeamPage.scss';
+
+function TeamPage() {
+    return (
+        <div>
+            <p>This is the TeamPage</p>
+        </div>
+    );
+}
+
+export default TeamPage;
