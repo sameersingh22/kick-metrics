ac96c0d9002b6e9e010af4cd62a4a8e60ea173b2
Sameer Singh <sameersingh1600@gmail.com>
2024-09-22 18:04:05 -0400

    Using the chart component

--- src/pages/TeamPage/TeamPage.jsx
+++ src/pages/TeamPage/TeamPage.jsx
@@ -1,33 +1,36 @@
-import React from "react";
-import { Link, useParams } from "react-router-dom";
+import React from 'react';
+import { Link, useParams } from 'react-router-dom';
+import PlayerGoalsBarChart from '../../components/PlayerGoalsBarChart/PlayerGoalsBarChart.jsx';
 import './TeamPage.scss';
 
 function TeamPage({ playersData }) {
     const { id } = useParams();
-    const teamId = parseInt(id, 10);
-    const players = playersData[teamId] || [];
+
+    const players = playersData[id] || [];
 
     return (
         <div className="team-page">
             <p className="team-page__nav">
-                <Link to="/">Landing Page</Link> &gt; <Link to="/home">Home Page</Link> &gt; Team {teamId} Page
+                <Link to="/">Landing Page</Link> &gt; <Link to="/home">Home Page</Link> &gt; Team {id} Page
             </p>
-            <h1>Team {teamId} Page</h1>
-            <ul className="team-page__list">
-                {players.length > 0 ? (
-                    players.map((player) => (
-                        <li key={player.id} className="team-page__player">
-                            <h3>{player.name}</h3>
-                            <p>Age: {player.age}</p>
-                            <p>Position: {player.position}</p>
-                            <Link to={`/playerMetrics/${player.id}`}>View Metrics</Link>
-                            <Link to={`/updatePlayer/${player.id}`}>Update Metrics</Link>
-                        </li>
-                    ))
-                ) : (
-                    <p>No players found for this team.</p>
-                )}
-            </ul>
+            <h1>Team {id} Page</h1>
+            {players.length > 0 ? (
+                <>
+                    <ul className="team__list">
+                        {players.map((player) => (
+                            <li key={player.id} className="team__player">
+                                <h3>{player.name}</h3>
+                                <p>Age: {player.age}</p>
+                                <p>Position: {player.position}</p>
+                                <Link to={`/playerMetrics/${player.id}`}>View Metrics</Link>
+                            </li>
+                        ))}
+                    </ul>
+                    <PlayerGoalsBarChart playersData={players} />
+                </>
+            ) : (
+                <p>No players found for this team.</p>
+            )}
         </div>
     );
 }
