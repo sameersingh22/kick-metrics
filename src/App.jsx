c83b87e82939167f390619c5f188bb752f9fc4d0
Sameer Singh <sameersingh1600@gmail.com>
2024-09-22 15:58:35 -0400

    Created backend and lilnked frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import axios from "axios";
 import Header from "./components/Header/Header";
 import HomePage from "./pages/HomePage/HomePage";
 import TeamPage from "./pages/TeamPage/TeamPage";
@@ -8,17 +9,20 @@ import PlayerMetricsPage from "./pages/PlayerMetricsPage/PlayerMetricsPage";
 import './styles/main.scss';
 
 function App() {
-    const playersData = {
-        1: [
-            { id: 1, name: "Player 1", age: 20, position: "Striker", goals: 10, assists: 5, teamId: 1 },
-            { id: 2, name: "Player 2", age: 22, position: "Left Mid", goals: 7, assists: 8, teamId: 1 },
-            { id: 3, name: "Player 3", age: 19, position: "Right Wing", goals: 9, assists: 6, teamId: 1 },
-        ],
-        2: [
-            { id: 4, name: "Player 4", age: 21, position: "Center Forward", goals: 15, assists: 3, teamId: 2 },
-            { id: 5, name: "Player 5", age: 23, position: "Right Mid", goals: 5, assists: 10, teamId: 2 },
-        ]
-    };
+    const [playersData, setPlayersData] = useState({});
+
+    useEffect(() => {
+        const fetchPlayersData = async () => {
+            try {
+                const response = await axios.get('http://localhost:8080/api/players');
+                setPlayersData(response.data);
+                console.log( response.data);
+            } catch (error) {
+            }
+        };
+
+        fetchPlayersData();
+    }, []);
 
     return (
         <BrowserRouter>
@@ -28,10 +32,13 @@ function App() {
                     <Routes>
                         <Route path="/" element={<LandingPage />} />
                         <Route path="/home" element={<HomePage />} />
-                        <Route path="/teams/:id" element={<TeamPage playersData={playersData} />} />
-                        <Route
-                            path="/playerMetrics/:id"
-                            element={<PlayerMetricsPage playersData={playersData} />}
+                        <Route 
+                            path="/teams/:id" 
+                            element={<TeamPage playersData={playersData} />} 
+                        />
+                        <Route 
+                            path="/playerMetrics/:id" 
+                            element={<PlayerMetricsPage playersData={playersData} />} 
                         />
                     </Routes>
                 </main>
