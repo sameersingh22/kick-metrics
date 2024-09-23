6358504003fdd4ee3128b450f7acd7350786c427
Sameer Singh <sameersingh1600@gmail.com>
2024-09-23 01:57:31 -0400

    Added image for HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import './HomePage.scss';
+import SoccerTeam from '../../assets/images/soccer-team.png';
 
 function HomePage() {
     return (
@@ -9,12 +10,15 @@ function HomePage() {
                 <Link to="/">Landing Page</Link> &gt; Home Page
             </p>
             <div className="home-page__teams">
-                <div className="home-page__card">
-                    <Link className="teamlink" to="/teams/1">Team 1</Link>
+                <div className="home-page__teams-content">
+                    <div className="home-page__card">
+                        <Link className="teamlink" to="/teams/1">Team 1</Link>
+                    </div>
+                    <div className="home-page__card">
+                        <Link className="teamlink" to="/teams/2">Team 2</Link>
+                    </div>
                 </div>
-                <div className="home-page__card">
-                    <Link className="teamlink" to="/teams/2">Team 2</Link>
-                </div>
+                <img src={SoccerTeam} alt="Soccer Team" className="home-page__image" />
             </div>
         </div>
     );
